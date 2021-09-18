0f2e924bf3a1485aa32ddbf6fe4e257070ef67e0
Xinchen Hou <65501463+Dora0523@users.noreply.github.com>
2021-09-17 21:09:38 -0400

    added hardcoded rankings for leaderboard

---
+++ ChefRisingStar/Models/ranking.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+
+namespace ChefRisingStar.Models
+{
+
+    [DebuggerDisplay("{GetDebuggerDisplay}")]
+    public class Rank
+    {
+        [JsonProperty("Ranking")]
+        public string Ranking { get; set; }
+
+        [JsonProperty("Recipe")]
+        public string Recipe { get; set; }
+
+        [JsonProperty("Rating")]
+        public string Rating { get; set; }
+
+        [JsonProperty("Link")]
+        public string Link { get; set; }
+    }
+}
---
+++ ChefRisingStar/ViewModels/RankingViewModel.cs
@@ -0,0 +1,49 @@
+﻿using ChefRisingStar.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Forms;
+
+
+namespace ChefRisingStar.ViewModels
+{
+    public class RankingViewModel : BaseViewModel
+    {
+   
+
+        public Rank rankData = new Rank();
+        public List<object> rankResultData = new List<object>();
+
+        public List<Rank> rankings
+        {
+            get => rankings;
+            set
+            {
+                if (rankings == value)
+                    return;
+
+                var response = File.ReadAllText("sample/ranking.json");
+
+
+                rankings = JsonConvert.DeserializeObject<List<Rank>>(response);
+            }
+        }
+
+        public RankingViewModel()
+        {
+            foreach (Rank r in rankings)
+            {
+                    rankResultData.Add(r);
+                
+
+            }
+                
+        }
+
+
+        }
+
+        
+    
+}
